f857407d013563a21a766407f3587ba6f36014aa
altundalferhat <100750736+altundalferhat@users.noreply.github.com>
2022-11-22 14:53:44 +0300

    Test

---
+++ server.js
@@ -0,0 +1,22 @@
+const os = require("os");
+const path = require("path");
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+console.log(os.networkInterfaces()["Wi-Fi"][1].address);
+console.log(__dirname);
+console.log(__filename);
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
+
+const fs = require("fs");
+
+fs.readFile("./text.txt", "utf8", (error, data) => {
+  console.log(data.toString());
+});
+fs.writeFile("./text.txt", "utf8", (error, data) => {
+  console.log((data = "Deneme"));
+});
